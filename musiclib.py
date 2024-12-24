52054e6d023de384f4607f6bb8cce028ef6b4fe3
Yauheni Zviazdou <ezvezdov22@gmail.com>
2024-12-24 16:12:08 +0100

    add get_track_info_genius,  change add_tag, change download_by_artist

--- musiclib.py
+++ musiclib.py
@@ -11,13 +11,17 @@ import logging
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import api_key
+import re 
 
+EXT = ".mp3"
 # Authenticate with Spotify
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=api_key.spotify_client_id, client_secret=api_key.spotify_client_secret))
 
 yt_dlp_download_options = {
     'format': 'bestaudio/best',  # Select the best audio format available
-    'outtmpl': '%(artist)s<|>%(track)s.%(ext)s',  # Custom output template
+    'outtmpl': '%(id)s.%(ext)s',  # Custom output template
+    # 'skip_download': True,  # This enables the --skip-download behavior
+    'retries': 5,  # Retry 5 times for errors
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',  # Set preferred codec to MP3
@@ -39,6 +43,10 @@ logging.basicConfig(
     filename='.musiclib.log',   # Log to file, omit for console logging
     filemode='w'          # Overwrite ('w') or append ('a') to log file
 )
+
+def trackname_remove_feat(title):
+    name = re.sub(r'\(feat.*?\)|\(ft.*?\)', '', title)
+    return name.rstrip()
 
 
 def get_lyrics(track_name, artists_names):
@@ -85,7 +93,7 @@ def get_lyrics(track_name, artists_names):
     logging.info(f"Lyrics: {lyrics_type} lyrics saved for {artists_names} - {track_name}")
     return lrc.rstrip()
 
-def get_track_info(track_name, artist_name):
+def get_track_info_spotify(track_name, artist_name):
     """
     Retrieves detailed information about a specific track.
 
@@ -125,7 +133,7 @@ def get_track_info(track_name, artist_name):
         track = results['tracks']['items'][0]
         album = track.get('album', {})
 
-        track_info['track_name'] = track.get('name', '')
+        track_info['track_name'] = trackname_remove_feat(track.get('name', ''))
         track_info['track_artists'] = [artist.get('name', '') for artist in track.get('artists', [])]
         track_info['album_name'] = album.get('name', '')
         track_info['release_date'] = album.get('release_date', '')
@@ -139,18 +147,44 @@ def get_track_info(track_name, artist_name):
         track_info['thumbnail_url'] = images[0].get('url', '') if images else ''
 
         # Logging
-        logging.info("Track Name:", track_info['track_name'])
-        logging.info("Artist(s):", track_info['track_artists'])
-        logging.info("Album:", track_info['album_name'])
-        logging.info("Release date:", track_info['release_date'])
-        logging.info("Track number:", track_info['track_number'])
-        logging.info("Total tacks:", track_info['total_tracks'])
-        logging.info("Album artists:", track_info['album_artists'])
-        logging.info("Thumbnail:", track_info['thumbnail_url'])
+        # logging.info("Track Name:", track_info['track_name'])
+        # logging.info("Artist(s):", track_info['track_artists'])
+        # logging.info("Album:", track_info['album_name'])
+        # logging.info("Release date:", track_info['release_date'])
+        # logging.info("Track number:", track_info['track_number'])
+        # logging.info("Total tacks:", track_info['total_tracks'])
+        # logging.info("Album artists:", track_info['album_artists'])
+        # logging.info("Thumbnail:", track_info['thumbnail_url'])
     else:
         logging.warning(f"Track {artist_name} - {track_name} was not found.")
     
     return track_info
+
+def get_track_info_genius(track_name, artists_names):
+    search_term = f"{artists_names} - {track_name}"
+    genius_search_url = f"http://api.genius.com/search?q={search_term}&access_token={api_key.genius_client_access_token}"
+
+    response = requests.get(genius_search_url)
+    json_data = response.json()
+
+    track = json_data['response']['hits'][0]['result']
+
+    track_info = dict()
+
+    track_info['track_name'] = trackname_remove_feat(track.get('title', ''))
+    track_info['track_artists'] = [track['primary_artist']['name']]
+    track_info['album_name'] = ""
+    track_info['release_date'] = f"{track['release_date_components']['year']} - {track['release_date_components']['month']} - {track['release_date_components']['day']}"
+
+    track_info['track_number'] = track.get('track_number', -1)
+    track_info['total_tracks'] = -1
+    track_info['album_artists'] = track_info['track_artists']
+    track_info['lyrics'] = get_lyrics(track_info['track_name'], ", ".join(track_info['track_artists']))
+
+    track_info['thumbnail_url'] = track['header_image_url']
+
+    return track_info
+
 
 def add_tag(audio_path, track_info):
     """
@@ -186,16 +220,19 @@ def add_tag(audio_path, track_info):
 
     # Add or update tags
     audio['TIT2'] = TIT2(encoding=3, text=track_info['track_name'])  # Track Name
-    audio['TPE1'] = TPE1(encoding=3, text="|".join(track_info['track_artists']))  # Track Artists
+    audio['TPE1'] = TPE1(encoding=3, text="/".join(track_info['track_artists']))  # Track Artists
+    print("track_artists",track_info['track_artists'])
     audio['TDRC'] = TDRC(encoding=3, text=track_info['release_date'])  # Release Date
 
 
-    if track_info['track_number'] > 0 and track_info['total_tracks'] > 0:
+    if track_info['total_tracks'] > 1:
         audio['TALB'] = TALB(encoding=3, text=track_info['album_name'])  # Album Name
-        audio['TXXX:Album Artist'] = TXXX(encoding=3, desc='Album Artist', text=", ".join(track_info['album_artists']))  # Album Artists
+        audio['TXXX:Album Artist'] = TXXX(encoding=3, desc='Album Artist', text="/".join(track_info['album_artists']))  # Album Artists
         audio['TRCK'] = TRCK(encoding=3, text=f'{track_info['track_number']}/{track_info['total_tracks']}')  # Track Number / Total Tracks
-    elif track_info['track_number'] > 0:
-        audio['TRCK'] = TRCK(encoding=3, text=f'{track_info['track_number']}')  # Track Number
+    # elif track_info['track_number'] > 0:
+    #     audio['TALB'] = TALB(encoding=3, text=track_info['album_name'])  # Album Name
+    #     audio['TXXX:Album Artist'] = TXXX(encoding=3, desc='Album Artist', text=", ".join(track_info['album_artists']))  # Album Artists
+    #     audio['TRCK'] = TRCK(encoding=3, text=f'{track_info['track_number']}')  # Track Number
     
     audio['USLT'] = USLT(encoding=3, lang='eng', desc='', text=track_info['lyrics'])  # Lyrics
 
@@ -224,7 +261,7 @@ def tmp(path):
         if f.is_file():
             name = f.name[:-4]
             info = name.split("<|>")
-            inf = get_track_info(info[-1], info[0])
+            inf = get_track_info_spotify(info[-1], info[0])
             add_tag(f.path,inf)
 
 def search_and_add_lyrics(audio_path):
@@ -237,14 +274,15 @@ def search_and_add_lyrics(audio_path):
     
 
     if track_name is None or artists is None:
-        print("ERROR: Unknown title or Artist!")
+        logging.error("ERROR: Unknown title or Artist!")
         return
 
     track_name = track_name.text[0]
     artists_names = ", ".join(artists.text)
 
     # Search for synced lyrics
-    lrc = syncedlyrics.search(f"{artists_names} {track_name}", providers=[ 'Lrclib', 'NetEase'],enhanced=True)
+    # lrc = syncedlyrics.search(f"{artists_names} {track_name}", providers=[ 'Lrclib', 'NetEase'],enhanced=True)
+    lrc = syncedlyrics.search(f"{artists_names} {track_name}")
 
     # There is only plain lyrics
     if lrc is None:
@@ -282,44 +320,121 @@ def add_lyrics_all(library_path):
     for f in os.scandir(library_path):
         if f.is_dir():
             add_lyrics_all(f.path)
-        elif f.name.lower().endswith(".mp3"):
+        elif f.name.lower().endswith(EXT):
             search_and_add_lyrics(f.path)
 
-def download_by_artist(artist_id):
+def download_by_artist(artist_id, download_folder):
     url = f"https://music.youtube.com/channel/{artist_id}"
     with yt_dlp.YoutubeDL(yt_dlp_download_options) as ydl:
-        ydl.download([url])
+        # ydl.download([url])
+        channel_metadata = ydl.extract_info(url, download=True)  # Get metadata without downloading
+
+        if not 'entries' in channel_metadata: return
+
+        video_metadata_list = []
+        for entry in channel_metadata['entries']:
+
+
+            entry_title = trackname_remove_feat(entry['title'])
+            entry_artists = ", ".join(entry['artists'])
+
+            print("entry_title", entry_title)
+            print("entry_artists", entry['artists'][0])
+
+            track_info = get_track_info_spotify(entry_title, entry_artists)
+            
+            if not track_info:
+                track_info_genius = get_track_info_genius(entry_title, entry_artists)
+            
+                if track_info_genius:
+                    track_info = track_info_genius
+                    track_info_spotify = get_track_info_spotify(track_info_genius['track_name'], ", ".join(track_info_genius['track_artists']))
+                    if track_info_spotify:
+                        track_info = track_info_spotify
+
+            
+            # Process the video (e.g., download or extract metadata)
+            # ydl.download([track_url])
+
+            
+
+            file_path = os.path.join(library_path, entry['id']+EXT)
+            new_filename = ", ".join(track_info['track_artists']) + " - " + track_info['track_name'] + EXT
+
+            new_path = os.path.join(library_path, track_info['track_artists'][0], new_filename)
+            if track_info['total_tracks'] > 1:
+                new_filename = f"{track_info['track_number']}. {new_filename}"
+                release_year = track_info['release_date'].split("-")[0]
+                new_path = os.path.join(library_path, track_info['track_artists'][0], f"[{release_year}] {track_info['album_name']}", new_filename)
+
+            add_tag(file_path,track_info)
+
+            os.makedirs(os.path.dirname(new_path), exist_ok=True)
+            os.rename(file_path, new_path)
+
+
+        return video_metadata_list
+
+
+
 
 
 
 if __name__ == "__main__":
-    # Define library folder
-    library_path = "/home/ezvezdov/Music"
+    library_path = input("Please enter the path for music library: ").strip()
+    
+    # Ensure the path ends with a slash (optional)
+    library_path = os.path.join(library_path, '')
 
-    for artist_dir in os.scandir(library_path):
+    # Create the directory
+    try:
+        os.makedirs(library_path, exist_ok=True)
+        logging.info(f"Folders created successfully at: {library_path}")
+    except Exception as e:
+        logging.warning(f"Error creating folders: {e}")
 
-        # Skip non-artist
-        if not artist_dir.is_dir(): continue
+    yt_dlp_download_options['outtmpl'] = os.path.join(library_path,yt_dlp_download_options['outtmpl'])
+    
+    # download_by_artist("UCnh49MovlpQD702w35IqU-Q", library_path) # BABANGIDA
+    # download_by_artist("UCzx6KkjhuWLZa_0gt_Y3MAQ", library_path) # LAPA
+    # download_by_artist("UCPfUH32WlUYohFvTqo65GBA", library_path) # Aneezy
+    # download_by_artist("UCxtUPmgn4pinQhVHLdKau9A", library_path) # Dima Ermuzevic
+    download_by_artist("UCqhjJO7w2rv5Tkk7ZFYl7QA", library_path) # Big Baby Tape
 
-        # Set variables
-        artist_name = artist_dir.name
-        album_artist = artist_name
+    
+    # tmp(library_path)
 
-        for album_dir in os.scandir(artist_dir):
-            # Skip singles
-            if not album_dir.is_dir(): continue
+    # for artist_dir in os.scandir(library_path):
 
-            print(album_dir.path)
-            for album_track in os.scandir(album_dir):
-                if album_track.name.lower().endswith(".mp3"):
-                    file_path = album_track.path
-                    try:
-                        audio = EasyID3(file_path)
-                    except ID3NoHeaderError:
-                        audio = EasyID3()  # Create if missing
-                        audio.save(file_path)
+    #     # Skip non-artist
+    #     if not artist_dir.is_dir(): continue
+
+    #     # Set variables
+    #     artist_name = artist_dir.name
+    #     album_artist = artist_name
+
+    #     for album_dir in os.scandir(artist_dir):
+    #         # Skip singles
+    #         if not album_dir.is_dir(): continue
+
+    #         print(album_dir.path)
+    #         for album_track in os.scandir(album_dir):
+    #             if album_track.name.lower().endswith(".mp3"):
+    #                 file_path = album_track.path
+    #                 try:
+    #                     audio = EasyID3(file_path)
+    #                 except ID3NoHeaderError:
+    #                     audio = EasyID3()  # Create if missing
+    #                     audio.save(file_path)
                         
 
-                    audio['albumartist'] = album_artist
-                    audio.save()
-                    print(f"Updated: {album_track.name}")
+    #                 audio['albumartist'] = album_artist
+    #                 audio.save()
+    #                 print(f"Updated: {album_track.name}")
+
+
+        # print(info)
+        # # Write JSON to file
+        # import json
+        # with open("data.json", "w") as json_file:
+        #     json.dump(info, json_file, indent=4)  # Use indent=4 for pretty-printing
