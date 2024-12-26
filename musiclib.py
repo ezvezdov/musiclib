cf4b05986940dcc78e169ee50c5f628b08c5fce9
Yauheni Zviazdou <ezvezdov22@gmail.com>
2024-12-26 03:02:12 +0100

    Make MusiclibS class for Spotify usage

--- musiclib.py
+++ musiclib.py
@@ -1,6 +1,4 @@
 import os
-import time
-import requests
 import logging
 import re
 
@@ -16,11 +14,6 @@ import tag_utils
 
 
 EXT = ".mp3"
-# Authenticate with Spotify
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=api_key.spotify_client_id, client_secret=api_key.spotify_client_secret))
-
-
-
 
 # Configure basic logging
 logging.basicConfig(
@@ -158,64 +151,76 @@ class Musiclib():
             ydl.download([track_url])
 
 
+class MusiclibS(Musiclib):
+    def __init__(self, library_path):
+        super().__init__(library_path)
 
-def get_track_info_spotify(track_name, artist_name):
-    """
-    Retrieves detailed information about a specific track.
+        # Authenticate with Spotify
+        self.sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=api_key.spotify_client_id, client_secret=api_key.spotify_client_secret))
 
-    Searches for the track using the provided track name and artist name, fetches 
-    metadata such as album details, release date, track number, and thumbnail URL.
-    Also attempts to fetch lyrics for the track.
+    def get_another_metadata(self, title, artists):
+        return self.get_track_info_spotify(title, artists)
 
-    Args:
-        track_name (str): The name of the track.
-        artist_name (str): The name of the artist.
+    def get_track_info_spotify(self, track_name, artist_name):
+        """
+        Retrieves detailed information about a specific track.
 
-    Returns:
-        dict: A dictionary containing track information with the following keys:
-            - `track_name` (str): Name of the track.
-            - `track_artists` (list[str]): List of artists who performed the track.
-            - `album_name` (str): Name of the album containing the track.
-            - `release_date` (str): Release date of the album.
-            - `track_number` (int): Track's position in the album.
-            - `total_tracks` (int): Total number of tracks in the album.
-            - `album_artists` (list[str]): List of artists credited for the album.
-            - `lyrics` (str): Lyrics of the track.
-            - `thumbnail_url` (str): URL of the album's thumbnail image.
-    """
-    logging.debug(f"Get information about track: {artist_name} - {track_name}")
+        Searches for the track using the provided track name and artist name, fetches 
+        metadata such as album details, release date, track number, and thumbnail URL.
+        Also attempts to fetch lyrics for the track.
 
-    # Construct the query
-    query = f"track:{track_name} artist:{artist_name}"
-    results = sp.search(q=query, type="track", limit=1)
+        Args:
+            track_name (str): The name of the track.
+            artist_name (str): The name of the artist.
 
-    track_info = dict()
+        Returns:
+            dict: A dictionary containing track information with the following keys:
+                - `track_name` (str): Name of the track.
+                - `track_artists` (list[str]): List of artists who performed the track.
+                - `album_name` (str): Name of the album containing the track.
+                - `release_date` (str): Release date of the album.
+                - `track_number` (int): Track's position in the album.
+                - `total_tracks` (int): Total number of tracks in the album.
+                - `album_artists` (list[str]): List of artists credited for the album.
+                - `lyrics` (str): Lyrics of the track.
+                - `thumbnail_url` (str): URL of the album's thumbnail image.
+        """
+        logging.debug(f"Get information about track: {artist_name} - {track_name}")
 
-    if results.get('tracks', {}).get('items', []):
-        track = results['tracks']['items'][0]
-        album = track.get('album', {})
+        # Construct the query
+        query = f"track:{track_name} artist:{artist_name}"
+        results = self.sp.search(q=query, type="track", limit=1)
 
-        track_info['track_name'] = trackname_remove_unnecessary(track.get('name', ''))
-        track_info['track_artists'] = [artist.get('name', '') for artist in track.get('artists', [])]
-        track_info['album_name'] = album.get('name', '')
-        track_info['release_date'] = album.get('release_date', '')
-        track_info['track_number'] = track.get('track_number', -1)
-        track_info['total_tracks'] = album.get('total_tracks', -1)
-        track_info['album_artists'] = [artist.get('name', '') for artist in album.get('artists', [])]
-        track_info['lyrics'] = lyrics_utils.get_lyrics(track_info['track_name'], ", ".join(track_info['track_artists']))
+        track_info = dict()
 
-        # Safely get the thumbnail URL
-        images = album.get('images', [])
-        track_info['thumbnail_url'] = images[0].get('url', '') if images else ''
+        if results.get('tracks', {}).get('items', []):
+            track = results['tracks']['items'][0]
+            album = track.get('album', {})
 
-    else:
-        logging.warning(f"Track {artist_name} - {track_name} was not found.")
-    
-    return track_info
+            track_info['track_name'] = trackname_remove_unnecessary(track.get('name', ''))
+            track_info['track_artists'] = [artist.get('name', '') for artist in track.get('artists', [])]
+            track_info['album_name'] = album.get('name', '')
+            track_info['release_date'] = album.get('release_date', '')
+            track_info['track_number'] = track.get('track_number', -1)
+            track_info['total_tracks'] = album.get('total_tracks', -1)
+            track_info['album_artists'] = [artist.get('name', '') for artist in album.get('artists', [])]
+            track_info['lyrics'] = lyrics_utils.get_lyrics(track_info['track_name'], ", ".join(track_info['track_artists']))
+
+            # Safely get the thumbnail URL
+            images = album.get('images', [])
+            track_info['thumbnail_url'] = images[0].get('url', '') if images else ''
+
+        else:
+            logging.warning(f"Track {artist_name} - {track_name} was not found.")
+        
+        return track_info
 
 
 if __name__ == "__main__":
     library_path = input("Please enter the path for music library: ").strip()
 
-    muslib = Musiclib(library_path)
-    muslib.download_artist_disocgrapy("Big Baby Tape", library_path) # Big Baby Tape
+    # muslib = Musiclib(library_path)
+    # muslib.download_artist_disocgrapy("Big Baby Tape", library_path) # Big Baby Tape
+
+    muslibS = MusiclibS(library_path)
+    muslibS.download_artist_disocgrapy("Big Baby Tape", library_path) # Big Baby Tape
