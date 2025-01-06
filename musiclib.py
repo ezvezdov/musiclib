daadb1db78ea4b374df98f1c055bb2550cce8e40
Yauheni Zviazdou <ezvezdov22@gmail.com>
2025-01-06 18:19:40 +0100

    MusiclibS: add download_track_by_name

--- musiclib.py
+++ musiclib.py
@@ -457,6 +457,31 @@ class MusiclibS(Musiclib):
 
         for track_info in tracks_metadata:
             self._download_track_by_metdata(track_info)
+
+    def download_track_by_name(self, artist_name, track_name, download_top_result=False):
+        logging_utils.logging.debug(f"Get information about track: {artist_name} - {track_name}")
+
+        # Construct the query
+        query = f"track:{track_name} artist:{artist_name}"
+        results = self.sp.search(q=query, type="track", limit=20)
+
+        for album in results['tracks']['items']:
+
+            track_name = album['name']
+            track_artists = [artist['name'] for artist in album['artists']]
+            track_artists_str = ", ".join(track_artists)
+
+            if not download_top_result:
+                track_full_name = track_artists_str + " - " + track_name
+                answer = input(f"Did you search track {track_full_name}? [y/n]: ")
+
+                # Skip current album
+                if answer.lower()[0] != 'y': continue
+            
+            album_metadata = self._get_album_metadata(album['album'])
+            track_info = album_metadata[album['track_number']-1]
+            self._download_track_by_metdata(track_info)
+
     
     def _download_track_by_metdata(self, track_info):
         search_term = f"{track_info['track_artists_str']} - {track_info['track_name']}"
